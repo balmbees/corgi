46e68851baca5941aac325f7fdb6902f20996ea8
Kurt <breath103@gmail.com>
2018-05-28 11:31:44 +0900

    v1.14.0 release Refactor Namespace / Middleware Relations

---
+++ src/__test__/e2e/middleware_api_spec.ts
@@ -0,0 +1,77 @@
+import {
+  Response,
+  Route,
+  Namespace,
+  Routes,
+  Router,
+  Parameter,
+  Middleware,
+  MiddlewareAfterOptions,
+} from '../../index';
+import * as Joi from 'joi';
+
+
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+
+describe("Calling complex middleware connected API", () => {
+  it("should run middleware after exception handler", async () => {
+    const router = new Router(
+      [
+        new Namespace('/api/:userId', {
+          params: {
+            userId: Joi.number()
+          },
+          async exceptionHandler(error: Error) {
+            // 2
+            return this.json(
+              {
+                error: error.message,
+              },
+              500
+            );
+          },
+          children: [
+            new Route({
+              path: '/followers',
+              method: 'GET',
+              operationId: "getFollowers",
+              desc: 'List of users that following me',
+              handler: async function () {
+                // 1
+                throw new Error(`Error: ${this.params.userId}`);
+              }
+            }),
+          ]
+        })
+      ], {
+        middlewares: [
+          new (class TestMiddleware extends Middleware<void> {
+            public async after(options: MiddlewareAfterOptions<void>): Promise<Response> {
+              const { response } = options;
+              // 3
+              if (response.statusCode === 500) {
+                response.headers["error"] = "XX";
+              }
+              return options.response;
+            }
+          })()
+        ]
+      }
+    );
+    const res = await router.resolve({
+      path: "/api/33/followers",
+      httpMethod: 'GET'
+    } as any, { timeout: 10000 });
+
+    chai.expect(res).to.deep.eq({
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'error': 'XX'
+      },
+      body: JSON.stringify({ error: `Error: 33`}),
+    });
+  });
+});
