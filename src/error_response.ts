977e7fa0670281829877c45e3464492c9f24ff92
Kurt <breath103@gmail.com>
2018-10-30 15:22:11 +0900

    encrypt and return error stacks

--- src/error_response.ts
+++ src/error_response.ts
@@ -1,3 +1,16 @@
+export class StandardError extends Error {
+  constructor(
+    public readonly statusCode: number,
+    public readonly options: {
+      code: string,
+      message: string,
+      metadata?: object,
+    }
+  ) {
+    super()
+  }
+}
+
 export interface StandardErrorResponseBody {
   error: {
     id?: string;
@@ -13,5 +26,57 @@ export interface StandardErrorResponseBody {
      * Human readable Error message, such as "You're not allowed to do this"
      */
     message: string;
+  }
+}
+
+import * as crypto from "crypto";
+const CipherAlgorithm = "aes-256-ctr";
+
+export class ErrorResponseFormatter {
+  // If Password is provided, it will show detailed information (encrypted)
+  constructor(private password: string | undefined) {
+    console.log("ErrorResponseFormatter : ", password);
+  }
+
+  public format(error: Error) {
+    if (error instanceof StandardError) {
+      return {
+        code: error.options.code,
+        message: error.options.message,
+        metadata: error.options.metadata,
+      };
+    } else {
+      // For Non-Standard error,
+      return {
+        code: error.name,
+        message: error.message,
+        metadata: (() => {
+          console.log("Password : ", this.password);
+          if (this.password) {
+            const cipher = crypto.createCipher(CipherAlgorithm, this.password)
+            return [
+              cipher.update(JSON.stringify({
+                name: error.name,
+                message: error.message,
+                stack: (error.stack || "").split("\n"),
+              }), 'utf8', 'hex'),
+              cipher.final('hex')
+            ].join("");
+          } else {
+            // Otherwise don't show metadata for security
+            return undefined;
+          }
+        })(),
+      };
+    }
+  }
+
+  public decryptErrorMetadata(message: string) {
+    const decipher = crypto.createDecipher(CipherAlgorithm, this.password);
+    const payload = JSON.parse([
+      decipher.update(message, 'hex', 'utf8'),
+      decipher.final('utf8'),
+    ].join(""));
+    return payload;
   }
 }
